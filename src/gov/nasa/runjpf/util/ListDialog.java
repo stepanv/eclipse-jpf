c1bde9c207481025348ab49d4ec3f349e1402d65
Peter Mehlitz <Peter.C.Mehlitz@nasa.gov>
2011-11-03 01:25:01 -0700

    standalone JPF project page with complete data entry
    little ListDialog utility
    
    added required jars

---
+++ src/gov/nasa/runjpf/util/ListDialog.java
@@ -0,0 +1,145 @@
+package gov.nasa.runjpf.util;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+
+public class ListDialog extends Dialog {
+
+  String title;
+  String message;
+  String[] items = new String[0];
+  
+  Shell shell;
+  Label infoLabel;
+  List list;
+  
+  String[] selection;
+    
+  public ListDialog (Shell parent) {
+    this(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL | SWT.RESIZE);
+  }
+  
+  public ListDialog (Shell parent, int style){
+    super( parent, style);
+  }
+
+  protected void createContents (final Shell shell){
+    int margin = 10;
+    FormData fd;
+    
+    shell.setLayout( new FormLayout());
+    
+    infoLabel = new Label(shell, SWT.NONE);
+    fd = new FormData();
+    fd.left = new FormAttachment( 0, margin);
+    fd.top = new FormAttachment( 0, margin);
+    infoLabel.setLayoutData(fd);
+    
+    infoLabel.setText( message);
+    
+    Button selectButton = new Button( shell, SWT.PUSH);
+    selectButton.setText("Select");
+    fd = new FormData();
+    fd.right = new FormAttachment(100, -margin);
+    fd.bottom = new FormAttachment( 100, -margin);
+    selectButton.setLayoutData(fd);
+    selectButton.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected (SelectionEvent e){
+        selection = list.getSelection();
+        if (selection == null){
+          setMessage("no item selected!");
+        } else {
+          shell.close();
+        }
+      }
+    });
+
+    Button cancelButton = new Button( shell, SWT.PUSH);
+    cancelButton.setText("Cancel");
+    fd = new FormData();
+    fd.right = new FormAttachment( selectButton, -margin);
+    fd.bottom = new FormAttachment( 100, -margin);
+    cancelButton.setLayoutData(fd);
+    cancelButton.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected (SelectionEvent e){
+        selection = null;
+        shell.close();
+      }
+    });
+    
+    list = new List(shell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+    fd = new FormData();
+    fd.left = new FormAttachment(0, margin);
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( infoLabel, margin);
+    fd.bottom = new FormAttachment( cancelButton, -margin);
+    list.setLayoutData(fd);
+    list.setItems(items);
+    
+    shell.setDefaultButton( selectButton);
+  }
+
+  public void setTitle (String title){
+    this.title = title;
+    if (shell != null){
+      shell.setText( message);
+    }
+  }
+
+  public void setMessage (String message){
+    this.message = message;
+    if (infoLabel != null){
+      infoLabel.setText( message);
+    }
+  }
+
+  public void setItems (String... items){
+    this.items = items;
+    if (list != null){
+      list.setItems( items);
+    }
+  }
+  
+  public void setItems(java.util.List<String> itemList){
+    String[] items = itemList.toArray( new String[itemList.size()]);
+    setItems( items);
+  }
+  
+  public String[] getSelection() {
+    return selection;
+  }
+      
+  public String[] open (){
+    shell = new Shell( getParent(), getStyle());
+    shell.setText( title);
+    createContents(shell);
+    shell.pack();
+    
+    Point pointerLoc = Display.getDefault().getCursorLocation();
+    shell.setLocation( pointerLoc.x, pointerLoc.y);
+    
+    shell.open();
+    
+    Display display = getParent().getDisplay();
+    while (!shell.isDisposed()){
+      if (!display.readAndDispatch()){
+        display.sleep();
+      }
+    }
+    
+    return getSelection();
+  }
+}
--- src/gov/nasa/runjpf/wizard/NewJPFProjectPage.java
+++ src/gov/nasa/runjpf/wizard/NewJPFProjectPage.java
@@ -1,108 +1,357 @@
 package gov.nasa.runjpf.wizard;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.runjpf.EclipseJPF;
+import gov.nasa.runjpf.EclipseJPFLauncher;
+import gov.nasa.runjpf.util.ListDialog;
+
+import org.eclipse.core.filesystem.IFileInfo;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.ui.dialogs.WorkingSetGroup;
-import org.eclipse.ui.internal.ide.dialogs.ProjectContentsLocationArea;
-import org.eclipse.ui.internal.ide.dialogs.ProjectContentsLocationArea.IErrorMessageReporter;
-import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.ide.dialogs.IDEResourceInfoUtils;
 import org.eclipse.ui.internal.ide.dialogs.ProjectContentsLocationArea.IErrorMessageReporter;
 
 public class NewJPFProjectPage extends WizardPage {
 
-  private static final int SIZING_TEXT_FIELD_WIDTH = 250;
-  private static String initialProjectFieldValue = "jpf-";
-
   Text projectNameField;
+  Button defaultLocButton;
+  Button browseLocButton;
+  Text projectPathField;
+  List depProjectsList;
+  List depJarsList;
 
-  private ProjectContentsLocationArea locationArea;
-  private WorkingSetGroup workingSetGroup;
-
+  String lastSitePath;
+  java.util.List<String> definedProjects;
+  
   public NewJPFProjectPage(String pageName) {
     super(pageName);
     setPageComplete(false);
   }
 
   @Override
-  public void createControl(Composite parent) {
-    Composite composite = new Composite(parent, SWT.NULL);
-
+  public void createControl (Composite parent){
     initializeDialogUnits(parent);
-
-    composite.setLayout(new GridLayout());
-    composite.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-    createProjectNameGroup(composite);
-    locationArea = new ProjectContentsLocationArea(getErrorReporter(),
-        composite);
-
-    // Scale the button based on the rest of the dialog
-    setButtonLayoutData(locationArea.getBrowseButton());
+    
+    Composite composite = createPageContent(parent);
 
     setPageComplete(validatePage());
-    // Show description on opening
+    
     setErrorMessage(null);
     setMessage(null);
+    
     setControl(composite);
-    Dialog.applyDialogFont(composite);
+    Dialog.applyDialogFont(composite);    
   }
+  
+  private Composite createPageContent (final Composite parent){
+    int margin = 10;
+    int margin2 = 15;
+    FormData fd;
+    GridLayout gridLayout;
+    Button button;
+    Group group;
+    List list;
+    Composite c;
+    
+    Composite composite = new Composite(parent, SWT.NULL);
+    composite.setLayout( new FormLayout());
+    
+    //--- the project name group
+    Label projectLabel = new Label( composite, SWT.NONE);
+    projectLabel.setText("Project name:");
+    fd = new FormData();
+    fd.left = new FormAttachment( 0, margin);
+    fd.top = new FormAttachment( 0, margin);
+    projectLabel.setLayoutData( fd);
+    
+    projectNameField = new Text(composite, SWT.BORDER);
+    fd = new FormData();
+    fd.left = new FormAttachment( projectLabel, margin);
+    fd.top = new FormAttachment( 0, margin);
+    fd.right = new FormAttachment( 100, -margin);
+    projectNameField.setLayoutData( fd);
+    
+    projectNameField.setText("jpf-");
+    projectNameField.addListener( SWT.Modify, new Listener(){
+      public void handleEvent(Event e) {
+        setPageComplete(validatePage());
+      }
+    });
+        
+    //--- the project location group
+    defaultLocButton = button = new Button(composite, SWT.CHECK | SWT.RIGHT);
+    button.setText("use default workspace");
+    button.setSelection(true);
+    fd = new FormData();
+    fd.left = new FormAttachment( 0, margin);
+    fd.top = new FormAttachment( projectLabel, margin2);
+    button.setLayoutData( fd);
+    
+    button.setSelection(true);
+    button.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent e) {
+        boolean useDefaults = defaultLocButton.getSelection();
+        if (useDefaults) {
+          projectPathField.setText(getDefaultPathDisplayString());
+          projectPathField.setEnabled(false);
+          browseLocButton.setEnabled(false);
+        } else {
+          projectPathField.setText("");
+          projectPathField.setEnabled(true);
+          browseLocButton.setEnabled(true);
+        }
+      }
+    });
 
-  private final void createProjectNameGroup(Composite parent) {
-    // project specification group
-    Composite projectGroup = new Composite(parent, SWT.NONE);
-    GridLayout layout = new GridLayout();
-    layout.numColumns = 2;
-    projectGroup.setLayout(layout);
-    projectGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+    Label locLabel = new Label( composite, SWT.NONE);
+    locLabel.setText("Location:");
+    fd = new FormData();
+    fd.left = new FormAttachment( 0, margin);
+    fd.top = new FormAttachment( defaultLocButton, margin);
+    locLabel.setLayoutData( fd);
 
-    // new project label
-    Label projectLabel = new Label(projectGroup, SWT.NONE);
-    projectLabel
-        .setText(IDEWorkbenchMessages.WizardNewProjectCreationPage_nameLabel);
-    projectLabel.setFont(parent.getFont());
+    browseLocButton = button = new Button( composite, SWT.PUSH);
+    button.setText("Browse..");
+    fd = new FormData();
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( locLabel, -5, SWT.TOP);
+    button.setLayoutData(fd);
+    
+    browseLocButton.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent event) {
+          handleLocationBrowseButtonPressed();
+      }
+    });
+    browseLocButton.setEnabled(false);
+    
+    projectPathField = new Text(composite, SWT.BORDER);
+    fd = new FormData();
+    fd.left = new FormAttachment(locLabel, margin);
+    fd.right = new FormAttachment(browseLocButton, -margin);
+    fd.top = new FormAttachment( defaultLocButton, margin);
+    projectPathField.setLayoutData( fd);
+    projectPathField.setText(getDefaultPathDisplayString());
+    projectPathField.setEnabled(false);
+    
+    //--- dependency projects
+    Group projGroup = group = new Group( composite, SWT.SHADOW_ETCHED_OUT);
+    group.setText("Project Dependencies");
+    group.setLayout( new FormLayout());
+    fd = new FormData();
+    fd.left = new FormAttachment(0, margin);
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( locLabel, margin2);
+    //fd.bottom = new FormAttachment(100, -margin);
+    group.setLayoutData( fd);
+    
+    c = new Composite( group, SWT.NONE);
+    gridLayout = new GridLayout();
+    gridLayout.marginHeight = gridLayout.marginWidth = 0;
+    c.setLayout(gridLayout);
+    fd = new FormData();
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( 0, margin);
+    c.setLayoutData(fd);
+    
+    button = new Button( c, SWT.PUSH);
+    button.setText("Add..");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        java.util.List<String> projects = getSiteProjects();
+        if (projects.isEmpty()){
+          setErrorMessage("no projects defined in site.properties");
+          return;
+        }
+        ListDialog dialog = new ListDialog(getShell());
+        dialog.setTitle("Select Dependency Project");
+        dialog.setMessage("projects defined in: " + getSitePropertiesPath());
+        dialog.setItems( getSiteProjects());
+        String[] selectedProjects = dialog.open();
+        if (selectedProjects != null){
+          for (String proj : selectedProjects){
+            if (depProjectsList.indexOf(proj) >= 0) {
+              setErrorMessage("project already selected");
+            } else {
+              depProjectsList.add(proj);
+            }
+          }
+        }
+      }
+    });
 
-    // new project name entry field
-    projectNameField = new Text(projectGroup, SWT.BORDER);
-    GridData data = new GridData(GridData.FILL_HORIZONTAL);
-    data.widthHint = SIZING_TEXT_FIELD_WIDTH;
-    projectNameField.setLayoutData(data);
-    projectNameField.setFont(parent.getFont());
+    button = new Button( c, SWT.PUSH);
+    button.setText("Remove");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        int[] indices = depProjectsList.getSelectionIndices();
+        depProjectsList.remove( indices);
+      }
+    });
+    
+    button = new Button( c, SWT.PUSH);
+    button.setText("Up");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        int index = depProjectsList.getSelectionIndex();
+        if (index > 0){
+          String project = depProjectsList.getItem(index);
+          depProjectsList.remove(index);
+          index--;
+          depProjectsList.add( project, index);
+          depProjectsList.setSelection(index);         
+        }
+      }
+    });
+    
+    button = new Button( c, SWT.PUSH);
+    button.setText("Down");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        int index = depProjectsList.getSelectionIndex();
+        int n = depProjectsList.getItemCount()-1;
+        if (index < n){
+          String project = depProjectsList.getItem(index);
+          depProjectsList.remove(index);
+          index++;
+          depProjectsList.add( project, index);
+          depProjectsList.setSelection(index);         
+        }
+      }
+    });
+    
+    depProjectsList = list = new List( group, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+    fd = new FormData();
+    fd.left = new FormAttachment(0, margin);
+    fd.right = new FormAttachment(c, -margin);
+    fd.top = new FormAttachment( 0, margin);
+    fd.bottom = new FormAttachment( 100, -margin);
+    list.setLayoutData(fd);
 
-    // Set the initial value first before listener
-    // to avoid handling an event during the creation.
-    if (initialProjectFieldValue != null) {
-      projectNameField.setText(initialProjectFieldValue);
+    //--- dependency libraries
+    Group jarGroup = group = new Group( composite, SWT.SHADOW_ETCHED_OUT);
+    group.setText("Jar Dependencies");
+    group.setLayout( new FormLayout());
+    fd = new FormData();
+    fd.left = new FormAttachment(0, margin);
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( projGroup, margin2);
+    fd.bottom = new FormAttachment(100, -margin);
+    group.setLayoutData( fd);
+ 
+    c = new Composite( group, SWT.NONE);
+    gridLayout = new GridLayout();
+    gridLayout.marginHeight = gridLayout.marginWidth = 0;
+    c.setLayout(gridLayout);
+    fd = new FormData();
+    fd.right = new FormAttachment(100, -margin);
+    fd.top = new FormAttachment( 0, margin);
+    c.setLayoutData(fd);
+    
+    button = new Button( c, SWT.PUSH);
+    button.setText("Add..");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        FileDialog dialog = new FileDialog(getShell());
+        dialog.setFilterExtensions( new String[] {"*.jar"});
+        String selectedJar = dialog.open();
+        if (selectedJar != null){
+          if (depJarsList.indexOf(selectedJar) >= 0){
+            setErrorMessage("jar already selected");
+            return;
+          }
+          depJarsList.add( selectedJar);
+        }
+      }
+    });
+
+    button = new Button( c, SWT.PUSH);
+    button.setText("Remove");
+    button.setLayoutData( new GridData( GridData.FILL_BOTH));
+    button.addSelectionListener( new SelectionAdapter(){
+      @Override
+      public void widgetSelected( SelectionEvent event){
+        int[] indices = depJarsList.getSelectionIndices();
+        depJarsList.remove( indices);
+      }
+    });
+
+    depJarsList = list = new List( group, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+    fd = new FormData();
+    fd.left = new FormAttachment(0, margin);
+    fd.right = new FormAttachment(c, -margin);
+    fd.top = new FormAttachment( 0, margin);
+    fd.bottom = new FormAttachment( 100, -margin);
+    list.setLayoutData(fd);
+    
+    return composite;
+  }
+  
+  private void handleLocationBrowseButtonPressed() {
+    String selectedDirectory = null;
+    String dirName = projectPathField.getText();
+
+    if (!dirName.isEmpty()) {
+      IFileInfo info;
+      info = IDEResourceInfoUtils.getFileInfo(dirName);
+      if (info == null || !(info.exists())) {
+        dirName = "";
+      }
     }
 
-    Listener nameModifyListener = new Listener() {
-      public void handleEvent(Event e) {
-        setLocationForSelection();
-        boolean valid = validatePage();
-        setPageComplete(valid);
+    DirectoryDialog dialog = new DirectoryDialog( projectPathField.getShell());
+    dialog.setMessage("Choose Project Location");
 
-      }
-    };
-    projectNameField.addListener(SWT.Modify, nameModifyListener);
+    dialog.setFilterPath(dirName);
+    selectedDirectory = dialog.open();
+
+    if (selectedDirectory != null){
+      projectPathField.setText(selectedDirectory);
+    }
   }
 
-  void setLocationForSelection() {
-    locationArea.updateProjectName(getProjectNameFieldValue());
+  
+  private String getDefaultPathDisplayString() {
+    return Platform.getLocation().toOSString();
   }
   
   private IErrorMessageReporter getErrorReporter() {
@@ -120,38 +369,76 @@ public class NewJPFProjectPage extends WizardPage {
     };
   }
 
-  private String getProjectNameFieldValue() {
-    if (projectNameField == null) {
-      return "";
-    }
-    return projectNameField.getText().trim();
-  }
-
-  public String getProjectName() {
-    if (projectNameField == null) {
-      return initialProjectFieldValue;
-    }
-
-    return getProjectNameFieldValue();
-  }
-
   public IProject getProjectHandle() {
-    return ResourcesPlugin.getWorkspace().getRoot()
-        .getProject(getProjectName());
+    return ResourcesPlugin.getWorkspace().getRoot().getProject(projectNameField.getText());
   }
 
+  //--- site.properties processing
+  
+  protected String getSitePropertiesPath() {
+    return EclipseJPF.getDefault().getPluginPreferences().getString(EclipseJPFLauncher.SITE_PROPERTIES_PATH);
+  }
+  
+  protected java.util.List<String> getSiteProjects(){
+    String sitePath = getSitePropertiesPath();
+    if (sitePath == null){
+      setErrorMessage("no site.properties");
+      return null;
+    }
+    
+    if (sitePath.equals(lastSitePath)){
+      return definedProjects;
+    }
+    
+    lastSitePath = sitePath;
+    File file = new File( getSitePropertiesPath());
+    
+    try {
+      FileReader fr = new FileReader(file);
+      Config config = new Config(fr);
+      java.util.List<String> projects = new ArrayList<String>();
+      
+      for (String projId : config.getEntrySequence()){
+        if ("extensions".equals(projId)){
+          // we have to filter this out in case there is only a single project in
+          // the list, in which case we find a jpf.properties under its value
+          continue;
+        }
+        
+        String v = config.getString(projId);
+        File projDir = new File(v);
+        
+        if (projDir.isDirectory()){
+          File propFile = new File(projDir, "jpf.properties");
+          if (propFile.isFile()){
+            projects.add(projId);
+          }
+        }
+      }
+      
+      definedProjects = projects;
+      return projects;
+      
+    } catch (FileNotFoundException e) {
+      setErrorMessage( "no site.properties found");
+      return null;
+    }
+  }
+  
+  
+  //--- page content validation
+  
   protected boolean validatePage() {
     IWorkspace workspace = IDEWorkbenchPlugin.getPluginWorkspace();
 
-    String projectFieldContents = getProjectNameFieldValue();
-    if (projectFieldContents.equals("")) { //$NON-NLS-1$
+    String projectFieldContents = projectNameField.getText();
+    if (projectFieldContents.equals("")) {
       setErrorMessage(null);
-      setMessage(IDEWorkbenchMessages.WizardNewProjectCreationPage_projectNameEmpty);
+      setMessage("no project name");
       return false;
     }
 
-    IStatus nameStatus = workspace.validateName(projectFieldContents,
-        IResource.PROJECT);
+    IStatus nameStatus = workspace.validateName(projectFieldContents, IResource.PROJECT);
     if (!nameStatus.isOK()) {
       setErrorMessage(nameStatus.getMessage());
       return false;
@@ -159,17 +446,12 @@ public class NewJPFProjectPage extends WizardPage {
 
     IProject handle = getProjectHandle();
     if (handle.exists()) {
-      setErrorMessage(IDEWorkbenchMessages.WizardNewProjectCreationPage_projectExistsMessage);
+      setErrorMessage("project exists");
       return false;
     }
 
-    IProject project = ResourcesPlugin.getWorkspace().getRoot()
-        .getProject(getProjectNameFieldValue());
-    locationArea.setExistingProject(project);
-
-    String validLocationMessage = locationArea.checkValidLocation();
-    if (validLocationMessage != null) { // there is no destination location
-                                        // given
+    String validLocationMessage = checkValidLocation();
+    if (validLocationMessage != null) { // no destination location
       setErrorMessage(validLocationMessage);
       return false;
     }
@@ -178,4 +460,14 @@ public class NewJPFProjectPage extends WizardPage {
     setMessage(null);
     return true;
   }
+  
+  public String checkValidLocation() {
+    String locationFieldContents = projectPathField.getText();
+    if (locationFieldContents.length() == 0) {
+        return "no project location specified";
+    }
+
+    return null;
+  }
+  
 }
